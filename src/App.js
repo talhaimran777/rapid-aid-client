90bc1c799aebe2ec5ddde823ee8a14216755eaa6
Talha Imran <talhaimran284@gmail.com>
2022-09-24 13:00:37 +0500

    Updated

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import { useDispatch } from 'react-redux'
 import Router from './router/Router'
 import useJwt from '@src/auth/jwt/useJwt'
 import jwt_decode from 'jwt-decode'
-import { io } from 'socket.io-client'
+import socketClient  from "socket.io-client";
+
 import { SET_CURRENT_USER, SET_IO } from './redux/actions/action.types/actionTypes'
 
+const SERVER = "https://creepy-toad-gaiters.cyclic.app";
 const App = () => {
   const dispatch = useDispatch()
-  const socket = io.connect("wss://creepy-toad-gaiters.cyclic.app")
+  var socket = socketClient (SERVER);
+      socket.on('connection', () => {
+          console.log(`I'm connected with the back-end`);
+  });
 
   if (useJwt.getToken()) {
     const token = useJwt.getToken()
